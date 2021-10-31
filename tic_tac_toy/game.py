4844f0399aeaaa478de4e7e801f8e89469c7c793
yulia <nappestok@gmail.com>
2021-10-31 22:46:28 +0300

    tic-tac-toy v 0.5

--- tic_tac_toy/game.py
+++ tic_tac_toy/game.py
@@ -3,6 +3,7 @@ from itertools import cycle
 from tic_tac_toy.board import get_board, board_match, display_board
 from tic_tac_toy.steps import make_step, ask_new_game
 from tic_tac_toy.users import ask_mode, create_users
+from logging import logger
 
 
 def game_init() -> dict:
@@ -17,6 +18,10 @@ def game_end(step_num, winner):
     result_str = f"победил {winner['name']}" if winner else "произвошла ничья"
     print(f"На ходу #{step_num}", result_str)
     user_answer = ask_new_game()
+
+    # log revenge
+    logger.log_revenge(user_answer)
+
     return user_answer
 
 
@@ -24,6 +29,9 @@ def game_cycle(users: list[dict, ...], board: list[list]):
     winner = None
     step_num = None
     steps = set()
+
+    logger.log_game_start(users[1]['mode'])
+
     for step_num, user in enumerate(cycle(users), 1):
         user["all_steps"] = steps
 
@@ -37,4 +45,8 @@ def game_cycle(users: list[dict, ...], board: list[list]):
             break
         if step_num > 8:
             break
+
+    # log game steps, name and step
+    logger.log_game_end(steps, winner, step_num)
+
     return step_num, winner
